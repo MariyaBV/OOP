497b19b24c76de99f7bb59b0c543c321e2c1c294
MariyaBV <mari.mv2008@gmail.com>
2019-02-26 00:03:06 +0300

    commit 2, fixed bugs

--- lab1/BinaryToDecimal/BinaryToDecimal/tests.bat
+++ lab1/BinaryToDecimal/BinaryToDecimal/tests.bat
@@ -6,16 +6,12 @@ set OUT="%TEMP%\out.txt"
 ::ѕри запуске без параметров ожидаетс€ ненулевой код возврата
 %PROGRAM% > nul
 if NOT ERRORLEVEL 1 goto err
+::exit /B 0
 
 ::ѕереданы не все аргументы
 %PROGRAM% > %OUT%
 if NOT ERRORLEVEL 1 goto err
 fc %OUT% incorrect_number_of_parameters.txt
-if ERRORLEVEL 1 goto err
-
-%PROGRAM% "10021" > %OUT% 
-if NOT ERRORLEVEL 1 goto err
-fc %OUT% is_not_binary_number.txt
 if ERRORLEVEL 1 goto err
 
 %PROGRAM% "1001" > %OUT% || goto err
@@ -24,6 +20,16 @@ if ERRORLEVEL 1 goto err
 
 %PROGRAM% "0" > %OUT% || goto err
 fc %OUT% zero.txt
+if ERRORLEVEL 1 goto err
+
+%PROGRAM% "10021" > %OUT% 
+if NOT ERRORLEVEL 1 goto err
+fc %OUT% invalid_digit.txt
+if ERRORLEVEL 1 goto err
+
+%PROGRAM% "11111111111111111111111111111111" > %OUT% 
+if NOT ERRORLEVEL 1 goto err
+fc %OUT% overflow.txt
 if ERRORLEVEL 1 goto err
 
 echo Program testing succeeded
