ccc0455b15bdf9575a3db662df49358a48cc860f
MariyaBV <mari.mv2008@gmail.com>
2019-03-12 19:30:40 +0300

    Change vector

--- lab2/СhangeArray/СhangeArray/main.cpp
+++ lab2/СhangeArray/СhangeArray/main.cpp
@@ -6,22 +6,11 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	vector<double> inputVector;
-
-	if (argc == 2)
-	{
-		ifstream input(argv[1]);
-		ReadVectorFromStream(input, inputVector);
-	}
-	else
-	{
-		ReadVectorFromStream(cin, inputVector);
-	}
-
+	vector<double> inputVector(istream_iterator<double>(cin), (istream_iterator<double>()));
 
 	if (inputVector.size() == 0)
 	{
-		cout << "Invalid input. /n"
+		cout << "Invalid input.\n"
 			 << "Usage: <double number> [use spaces between numbers]" << endl;
 		return 1;
 	}
