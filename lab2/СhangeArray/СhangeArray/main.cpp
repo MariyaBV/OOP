988cb3269c0f9e9fb844ff1189990c9fbd3be124
MariyaBV <mari.mv2008@gmail.com>
2019-03-14 21:10:08 +0300

    ChangeArray исправлены замечания

--- lab2/СhangeArray/СhangeArray/main.cpp
+++ lab2/СhangeArray/СhangeArray/main.cpp
@@ -8,12 +8,6 @@ int main(int argc, char* argv[])
 {
 	vector<double> inputVector(istream_iterator<double>(cin), (istream_iterator<double>()));
 
-	if (inputVector.size() == 0)
-	{
-		cout << "Invalid input.\n"
-			 << "Usage: <double number> [use spaces between numbers]" << endl;
-		return 1;
-	}
 	ProcessVector(inputVector);
 	sort(inputVector.begin(), inputVector.end());
 	PrintVector(inputVector);
