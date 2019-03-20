d2ab24848040103f74493caf4948db7ebb06811b
MariyaBV <mari.mv2008@gmail.com>
2019-03-20 16:38:54 +0300

    PrimeNumbersGenerator

---
+++ lab2/PrimeNumberGenerator/PrimeNumberGenerator/PrimeNumberGenerator.h
@@ -0,0 +1,6 @@
+#include "pch.h"
+
+using namespace std;
+
+set<int> GeneratePrimeNumbersSet(const unsigned int upperBound);
+void PrintPrimeNumbersSet(const set<int> PrimeNumbersSet);
---
+++ lab2/PrimeNumberGenerator/PrimeNumberGenerator/main.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+#include "PrimeNumberGenerator.h"
+
+using namespace std;
+const int MAX_UPPER_BOUND = 100000000;
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		cout << "Invalid arguments count\n"
+			 << "Usage: PrimeNumberGenerator.exe <unsigned upper bound>" << endl;
+
+		return 1;
+	}
+
+	unsigned int upperBound = stoul(argv[1]); //fabs(atoi());
+
+	if (upperBound > MAX_UPPER_BOUND)
+	{
+		cout << "Invalid upper bound\n"
+			 << "Usage: upper bound < " << MAX_UPPER_BOUND << endl;
+
+		return 1;
+	}
+
+	PrintPrimeNumbersSet(GeneratePrimeNumbersSet(upperBound));
+
+	return 0;
+}
