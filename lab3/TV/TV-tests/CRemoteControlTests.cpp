c8f218b400a051a3aaf23248c218e50fa42fa47d
MariyaBV <mari.mv2008@gmail.com>
2019-04-09 17:15:20 +0300

    CRemoteControlTests

--- lab3/TV/TV-tests/CRemoteControlTests.cpp
+++ lab3/TV/TV-tests/CRemoteControlTests.cpp
@@ -64,5 +64,38 @@ SCENARIO("Remote control provides information about TV", "[remote]")
 				CHECK(output.str() == "TV is turned on\nChannel is: 17\n");
 			}
 		}
+
+		string ort = "ORT";
+		string kanal24 = "24 kanal";
+		tv.SetChannelName(1, ort);
+		tv.SetChannelName(24, kanal24);
+		WHEN("user saved names of channels")
+		{
+			rc.HandleCommand();
+			THEN("it is notified that TV is on the current channel and all numbers of channel with name")
+			{
+				CHECK(output.str() == "TV is turned on\nChannel is: 17\n1 - ORT\n24 - 24 kanal\n");
+			}
+		}
+
+		tv.SetChannelName(30, ort);
+		WHEN("user saved name if the specified name has already been assigned to another channel")
+		{
+			rc.HandleCommand();
+			THEN("it is notified that TV is on the current channel and all numbers of channel with name")
+			{
+				CHECK(output.str() == "TV is turned on\nChannel is: 17\n24 - 24 kanal\n30 - ORT\n");
+			}
+		}
+
+		tv.DeleteChannelName(kanal24);
+		WHEN("delete channel")
+		{
+			rc.HandleCommand();
+			THEN("it is notified that TV is on the current channel and all numbers of channel with name")
+			{
+				CHECK(output.str() == "TV is turned on\nChannel is: 17\n30 - ORT\n");
+			}
+		}
 	}
 }
