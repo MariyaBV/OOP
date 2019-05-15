f7780ea272513d2ab9602f8797b3c33e1bdc3238
MariyaBV <mari.mv2008@gmail.com>
2019-05-15 17:59:15 +0300

    lab6 solve2 and tests for solve3

---
+++ lab6/SolutionOfEquationDegree4/SolutionOfEquationDegree4/EquationRoot4.cpp
@@ -0,0 +1,51 @@
+#include "pch.h"
+#include "EquationRoot4.h"
+
+using namespace std;
+
+QuadraticRoots Solve2(double a, double b, double c)
+{
+	QuadraticRoots root;
+	double x1, x2;
+	double D = pow(b, 2) - 4 * a * c;
+
+	if (D == 0)
+	{
+		root.root1 = - b / (2 * a);
+		root.root2 = root.root1;
+		root.rootFound = true;
+	}
+	else if (D > 0)
+	{
+		x1 = (-b + sqrt(D)) / (2 * a);
+		x2 = (-b - sqrt(D)) / (2 * a);
+
+		if (x1 < x2)
+		{
+			root.root1 = x1;
+			root.root2 = x2;
+		}
+		else
+		{
+			root.root1 = x2;
+			root.root2 = x1;
+		}
+		root.rootFound = true;
+	}
+
+	return root;
+}
+
+double Solve3(double a, double b, double c, double d)
+{
+	double maxValidSolution = 0;
+
+	return maxValidSolution;
+}
+
+//EquationRoot4 Solve4(double a, double b, double c, double d, double e)
+//{
+//	EquationRoot4 root;
+//
+//	return root;
+//};
---
+++ lab6/SolutionOfEquationDegree4/SolutionOfEquationDegree4/EquationRoot4.h
@@ -0,0 +1,18 @@
+#pragma once
+
+struct QuadraticRoots
+{
+	bool rootFound = false;
+	double root1 = 0;
+	double root2 = 0;
+};
+
+struct EquationRoot4
+{
+	double numRoots;
+	double roots[4];
+};
+
+QuadraticRoots Solve2(double a, double b, double c);
+double Solve3(double a, double b, double c, double d);
+//EquationRoot4 Solve4(double a, double b, double c, double d, double e);
