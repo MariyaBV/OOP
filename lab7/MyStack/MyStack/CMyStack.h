8d46226c3a03b53c68f58956c9079da413307d9e
MariyaBV <mari.mv2008@gmail.com>
2019-06-11 16:56:50 +0300

    myStack (pop,push,isEmpty,getTop)

---
+++ lab7/MyStack/MyStack/CMyStack.h
@@ -0,0 +1,70 @@
+#pragma once
+
+template <typename T>
+class CMyStack
+{
+	struct Component
+	{
+		T data;
+		Component* next;
+	};
+
+public:
+	CMyStack() = default;
+	/*~CMyStack()
+	{
+		Clear();
+	};*/
+	bool IsEmpty() const
+	{
+		return (m_top == NULL) ? true : false;
+	}
+
+	void Push(T const& newData)
+	{
+		try
+		{
+			Component* newComponent = new Component; //создаем и выделяем память для нового элемента
+			newComponent->data = newData; //записываем необходимую инфрмацию в data
+			newComponent->next = m_top; //проводим связь от нового элемента, к вершине
+			m_top = newComponent; //обозначаем, что вершиной теперь является новый элемент
+		}
+		catch (...)
+		{
+			throw;
+		}
+		
+	}
+
+	T GetTop() const
+	{
+		if (!IsEmpty())
+		{
+			return m_top->data;
+		}
+		else
+		{
+			throw std::out_of_range("Error. Empty stack.\n");
+		}
+	}
+
+	T Pop()
+	{
+		if (!IsEmpty())
+		{
+			T tempData = m_top->data;
+			Component* tempTop = m_top;
+			m_top = m_top->next;
+			free(tempTop);
+
+			return tempData;
+		}
+		else
+		{
+			throw std::out_of_range("Error. Empty stack.\n");
+		}
+	}
+
+private:
+	Component* m_top = nullptr;
+};
